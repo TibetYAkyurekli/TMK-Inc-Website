370997c4bc818431b379365728eba81010a625a1
Akyu0002 <akyu0002@algonquinlive.com>
2023-02-11 19:24:24 -0500

    Add body text to home screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,22 @@ function Home() {
       <TopNav />
       <Header />
       <HeroBanner />
+      <div className="homeBodyContainer">
+        <h2 className="homeBodyTitle">Who Are We?</h2>
+        <p className="homeBodyText">
+          We are a locally owned and operated tree service company providing
+          services to the Ottawa and surrounding area.
+        </p>
+        <p className="homeBodyText">
+          From small shrubs to large mature trees, TMK Inc staff have the
+          knowledge, skill, and equipment to handle any tree related projects.
+        </p>
+        <div className="homeBodyBtnContainer">
+          <button className="homeBodyBtn" href="#services">
+            See All Services Available
+          </button>
+        </div>
+      </div>
       <Footer />
     </>
   );
