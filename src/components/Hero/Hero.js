bd9887de937abdc7c417e5817845ed3291a56c99
Akyu0002 <akyu0002@algonquinlive.com>
2023-02-17 11:01:52 -0500

    Fix hero banner button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./hero.css";
+import "../../App.css";
 
 function HeroBanner() {
   return (
-    <div className="hero-banner">
+    <div className="heroBanner">
       <h1 className="heroTitle">TMK Tree and Hedge Inc.</h1>
       <div className="heroBodyContainer">
-        <h2 className="hero-body">Reliable</h2>
-        <h2 className="hero-body">Experienced</h2>
-        <h2 className="hero-body">Comprehensive</h2>
+        <h2 className="heroBody">Reliable</h2>
+        <h2 className="heroBody">Experienced</h2>
+        <h2 className="heroBody">Comprehensive</h2>
       </div>
       <div className="homeBodyBtnContainer">
         <button className="btn">
